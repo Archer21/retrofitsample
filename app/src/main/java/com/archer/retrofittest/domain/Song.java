97651a9e311ca93b492191d1b84fb6349ec2e0d9
Archer <yatogami18@gmail.com>
2016-02-02 11:13:51 -0400

    Agregado SongDeserializer retornando solo el nombre de la cancion

--- app/src/main/java/com/archer/retrofittest/domain/Song.java
+++ app/src/main/java/com/archer/retrofittest/domain/Song.java
@@ -1,10 +1,14 @@
 package com.archer.retrofittest.domain;
 
+import com.archer.retrofittest.io.model.JsonKeys;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Archer on 1/2/16.
  */
 public class Song {
 
+    @SerializedName(JsonKeys.SONG_NAME)
     private String name;
     private String urlSmallImage;
 
---
+++ app/src/main/java/com/archer/retrofittest/io/deserializer/SongDeserializer.java
@@ -0,0 +1,45 @@
+package com.archer.retrofittest.io.deserializer;
+
+import com.archer.retrofittest.domain.Song;
+import com.archer.retrofittest.io.model.JsonKeys;
+import com.archer.retrofittest.io.model.SongResponse;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+/**
+ * Created by Archer on 2/2/16.
+ */
+public class SongDeserializer implements JsonDeserializer<SongResponse> {
+    @Override
+    public SongResponse deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        Gson gson = new Gson();
+        SongResponse response = gson.fromJson(json, SongResponse.class);
+        JsonObject songsResponseData = json.getAsJsonObject().getAsJsonObject(JsonKeys.SONGS_RESULTS);
+        JsonArray  songArray = songsResponseData.getAsJsonArray(JsonKeys.SONGS_ARRAY);
+        response.setSongs(extractSongsFromJsonArray(songArray));
+
+    }
+    private ArrayList<Song> extractSongsFromJsonArray(JsonArray array)
+    {
+        ArrayList<Song> songs = new ArrayList<>();
+        int size = array.size();
+        for (int i = 0; i < size; i++) {
+            Song currentSong = new Song();
+
+            JsonObject songData = array.get(i).getAsJsonObject();
+            String name = songData.get(JsonKeys.SONG_NAME).getAsString();
+            currentSong.setName(name);
+            songs.add(currentSong);
+        }
+        return songs;
+    }
+
+}
