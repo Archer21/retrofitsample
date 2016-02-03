28e03a12736bf9316e73dde405ce8e1bf6268eee
Archer <yatogami18@gmail.com>
2016-02-03 13:09:00 -0400

    Agregada nueva URL_BASE

--- app/src/main/java/com/archer/retrofittest/io/ApiConstants.java
+++ app/src/main/java/com/archer/retrofittest/io/ApiConstants.java
@@ -5,11 +5,11 @@ package com.archer.retrofittest.io;
  */
 public class ApiConstants {
 
-//    https://api.myjson.com/bins/4dsrd
+//    https://api.myjson.com/bins/2qtzx
 
     public static final String URL_BASE = "https://api.myjson.com/";
     public static final String PATH_BIN = "bins/";
-    public static final String PARAM_SONGS = "4dsrd";
+    public static final String PARAM_SONGS = "2qtzx";
 
     public static final String URL_SONGS = PATH_BIN + PARAM_SONGS;
 
