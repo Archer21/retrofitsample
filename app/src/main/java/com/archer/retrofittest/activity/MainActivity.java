2ce9a872e6f41b9e9f2e8f2b142871458e78479a
Archer <yatogami18@gmail.com>
2016-02-03 10:12:39 -0400

    commit 1

--- app/src/main/java/com/archer/retrofittest/activity/MainActivity.java
+++ app/src/main/java/com/archer/retrofittest/activity/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -19,7 +18,7 @@ import com.archer.retrofittest.ui.fragments.SongsFragment;
 public class MainActivity extends AppCompatActivity{
 
     private DrawerLayout drawerLayout;
-    private String drawerTitle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
