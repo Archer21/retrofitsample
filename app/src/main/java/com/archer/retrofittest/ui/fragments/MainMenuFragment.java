c60f8d192cc8aba3376965e1cdf4b388542dae23
Archer <yatogami18@gmail.com>
2016-02-03 21:51:19 -0400

    Comenzando con MainMenuFragment

--- app/src/main/java/com/archer/retrofittest/ui/fragments/MainMenuFragment.java
+++ app/src/main/java/com/archer/retrofittest/ui/fragments/MainMenuFragment.java
@@ -1,7 +1,10 @@
 package com.archer.retrofittest.ui.fragments;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +13,10 @@ import com.archer.retrofittest.R;
 
 public class MainMenuFragment extends Fragment {
 
+    Toolbar toolbar;
+    ViewPager viewPager;
+    TabLayout tabLayout;
+
     public MainMenuFragment() {
         // Required empty public constructor
     }
@@ -17,7 +24,7 @@ public class MainMenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_main_menu, container, false);
+        return inflater.inflate(R.layout.content_main_menu, container, false);
     }
 
 }
