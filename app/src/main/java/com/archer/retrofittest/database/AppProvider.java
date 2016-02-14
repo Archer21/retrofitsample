f2ebc061d5a2c63f86aba014ead73c6438fd904c
Archer <yatogami18@gmail.com>
2016-02-14 14:25:34 -0400

    Arreglados cambios de FavoritesLoader

--- app/src/main/java/com/archer/retrofittest/database/AppProvider.java
+++ app/src/main/java/com/archer/retrofittest/database/AppProvider.java
@@ -5,35 +5,14 @@ import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-<<<<<<< HEAD
-import android.database.sqlite.SQLiteOpenHelper;
-import android.database.sqlite.SQLiteQueryBuilder;
-import android.net.Uri;
-import android.support.annotation.Nullable;
-
-public class AppProvider extends ContentProvider {
-
-    protected AppDatabase mOpenHelper;
-    private UriMatcher uriMatcher = buildUriMatcher();
-
-    private static final int FAVORITES = 100;
-    private static final int FAVORITES_ID = 101;
-
-    public static UriMatcher buildUriMatcher() {
-        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
-        String authority = FavoritesContracts.CONTENT_AUTHORITY;
-=======
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.provider.BaseColumns;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.annotation.StringDef;
 import android.text.TextUtils;
 
-/**
- * Created by Archer on 13/2/16.
- */
+
 public class AppProvider extends ContentProvider {
     protected AppDatabase mOpenHelper;
     public static final UriMatcher sUriMatcher = buildUriMatcher();
@@ -44,28 +23,17 @@ public class AppProvider extends ContentProvider {
     private static UriMatcher buildUriMatcher() {
         final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
         String authority = FavoritesContract.CONTENT_AUTHORITY;
->>>>>>> origin/addSqliteDatabase
         matcher.addURI(authority, "notes", FAVORITES);
         matcher.addURI(authority, "notes/*", FAVORITES_ID);
         return matcher;
     }
 
-<<<<<<< HEAD
-
-    public void deleteDatabase()
-    {
-=======
     private void deleteDatabase() {
->>>>>>> origin/addSqliteDatabase
         mOpenHelper.close();
         AppDatabase.deleteDatabase(getContext());
         mOpenHelper = new AppDatabase(getContext());
     }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/addSqliteDatabase
     @Override
     public boolean onCreate() {
         mOpenHelper = new AppDatabase(getContext());
@@ -74,15 +42,9 @@ public class AppProvider extends ContentProvider {
 
     @Nullable
     @Override
-<<<<<<< HEAD
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        final SQLiteDatabase db = mOpenHelper.getReadableDatabase();
-        final int match = uriMatcher.match(uri);
-=======
     public Cursor query(@NonNull Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
         final SQLiteDatabase database = mOpenHelper.getReadableDatabase();
         final int match = sUriMatcher.match(uri);
->>>>>>> origin/addSqliteDatabase
         SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
 
         switch (match){
@@ -91,36 +53,17 @@ public class AppProvider extends ContentProvider {
                 break;
             case FAVORITES_ID:
                 queryBuilder.setTables(AppDatabase.Tables.FAVORITES);
-<<<<<<< HEAD
-                String itemId = FavoritesContracts.Favorites.getFavoriteId(uri);
-                queryBuilder.appendWhere(itemId);
-=======
                 String favoriteId = FavoritesContract.Favorites.getFavoriteId(uri);
                 queryBuilder.appendWhere(BaseColumns._ID + "=" + favoriteId);
->>>>>>> origin/addSqliteDatabase
                 break;
             default:
                 throw new IllegalArgumentException("Unknown Uri: " + uri);
         }
-<<<<<<< HEAD
-
-        return queryBuilder.query(db, projection, selection, null, null, null, null, sortOrder);
-=======
         return queryBuilder.query(database, projection, selection, selectionArgs, null, null, sortOrder);
->>>>>>> origin/addSqliteDatabase
     }
 
     @Nullable
     @Override
-<<<<<<< HEAD
-    public String getType(Uri uri) {
-        final int match = uriMatcher.match(uri);
-        switch(match) {
-            case FAVORITES:
-                return FavoritesContracts.Favorites.CONTENT_TYPE;
-            case FAVORITES_ID:
-                return FavoritesContracts.Favorites.CONTENT_ITEM_TYPE;
-=======
     public String getType(@NonNull Uri uri) {
         final int match = sUriMatcher.match(uri);
         switch (match){
@@ -128,7 +71,6 @@ public class AppProvider extends ContentProvider {
                 return  FavoritesContract.Favorites.CONTENT_TYPE;
             case FAVORITES_ID:
                 return FavoritesContract.Favorites.CONTENT_ITEM_TYPE;
->>>>>>> origin/addSqliteDatabase
             default:
                 throw new IllegalArgumentException("Unknown Uri: " + uri);
         }
@@ -136,20 +78,6 @@ public class AppProvider extends ContentProvider {
 
     @Nullable
     @Override
-<<<<<<< HEAD
-    public Uri insert(Uri uri, ContentValues values) {
-        return null;
-    }
-
-    @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
-    }
-
-    @Override
-    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
-=======
     public Uri insert(@NonNull Uri uri, ContentValues values) {
         final SQLiteDatabase database = mOpenHelper.getWritableDatabase();
         final int match = sUriMatcher.match(uri);
@@ -202,6 +130,5 @@ public class AppProvider extends ContentProvider {
             default:
                 throw new IllegalArgumentException("Unknown Uri: " + uri);
         }
->>>>>>> origin/addSqliteDatabase
     }
 }
