5c8ba54d77adf2387a7350fbec3015c30382bd4c
Archer <yatogami18@gmail.com>
2016-02-14 19:10:29 -0400

    Provider implementado, myjson offline esperando para comprobar funcionamiento

--- app/src/main/java/com/archer/retrofittest/ui/fragments/FavoritesFragment.java
+++ app/src/main/java/com/archer/retrofittest/ui/fragments/FavoritesFragment.java
@@ -5,6 +5,8 @@ import android.content.ContentResolver;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,14 +14,20 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.archer.retrofittest.R;
+import com.archer.retrofittest.database.FavoritesLoader;
+import com.archer.retrofittest.domain.Song;
 import com.archer.retrofittest.ui.adapters.SongAdapter;
 
-public class FavoritesFragment extends Fragment{
+import java.util.ArrayList;
+
+public class FavoritesFragment extends Fragment implements LoaderManager.LoaderCallbacks<ArrayList<Song>>{
 
     private static final int NUM_COLS = 4;
     private RecyclerView mRecyclerView;
     private SongAdapter  mSongAdapter;
-
+    private ContentResolver mContentResolver;
+    private static final int LOADER_ID = 1;
+    private ArrayList<Song> mData;
 
     public FavoritesFragment() {
         // Required empty public constructor
@@ -34,16 +42,49 @@ public class FavoritesFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_favorites, container, false);
-
         mRecyclerView = (RecyclerView) root.findViewById(R.id.fragment_favorites_main_container);
         setConfig();
         return root;
     }
-    
+
 
     private void setConfig(){
+        mContentResolver = getActivity().getContentResolver();
+        setHasOptionsMenu(true);
+        getLoaderManager().initLoader(LOADER_ID, null, this);
+
         mRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), NUM_COLS));
         mRecyclerView.setAdapter(mSongAdapter);
+    }
+
+    @Override
+    public Loader<ArrayList<Song>> onCreateLoader(int id, Bundle args) {
+        mContentResolver = getActivity().getContentResolver();
+        return new FavoritesLoader(getActivity(), mContentResolver);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<ArrayList<Song>> loader, final ArrayList<Song> data) {
+//        Ease implementation first
+
+//        this.mData = data;
+//        Thread thread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                //This is not helpfull now but when we do searchs operations or long tasks
+//                //help us to dont block the ui thread
+//
+//                mSongAdapter.addAll(data);
+//            }
+//        });
+
+        mSongAdapter.addAll(data);
+        mData = data;
+    }
+
+    @Override
+    public void onLoaderReset(Loader<ArrayList<Song>> loader) {
+        mSongAdapter.addAll(null);
     }
 }
 
